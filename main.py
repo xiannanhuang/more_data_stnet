dcb4eaf73d9acd92fd618cf6a93798d9c7b3f0ef
xiannanhuang <139323946+xiannanhuang@users.noreply.github.com>
2024-01-31 18:55:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from train.train import train
-from train.self_distill import train_kd
 from train.kd_with_confid import train_kd_confid
 import os
 import logging
 import torch
-import numpy as np
-import time
-from datetime import datetime
 log_dir='./logs/nycbike'
 train_month=96
 model='STGCN'
